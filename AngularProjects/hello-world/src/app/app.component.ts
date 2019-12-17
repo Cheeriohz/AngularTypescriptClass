18bcf533afa56aafe6b23cfed5cd5194b79fbfc6
DougHomeDesktop <dwbono01@gmail.com>
2019-12-16 19:09:07 -0500

    updated hello world app to include working model

--- AngularProjects/hello-world/src/app/app.component.ts
+++ AngularProjects/hello-world/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Hello, World!';
+  givenName: string = 'Douglas Bonomo';
+  myModel: AppModel = null;
+
+  ngOnInit() {
+    //this.myModel = new AppModel("Gerico", 11);
+  }
+}
+
+class AppModel {
+  name:string = "";
+  age:number = 0;
+  constructor(name:string, age:number){
+    this.name = name;
+    this.age = age;
+  }
+  get toJson(): string {
+    return `{ name: ${this.name}, age: ${this.age} }`;
+  }
 }
