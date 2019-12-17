bee989e88a09c8e14b4101403935ba0ce0883786
max-student <max-student@maxtrain.com>
2019-12-17 13:10:23 -0500

    fixed binding on education component

--- AngularProjects/resume/src/app/education/education.component.ts
+++ AngularProjects/resume/src/app/education/education.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
-
+  educationHistory:EducationItem[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.educationHistory.push(new EducationItem({ description: 'North Oldham High School - Graduated' }));
+    this.educationHistory.push(new EducationItem({ description: 'UofL Bachelors of Computer Engineering', displayIdentifier: 'bold' }));
+    this.educationHistory.push(new EducationItem({ description: 'UofL Bachelors of Computer Science', displayIdentifier: 'bold' }));
   }
 
+}
+
+export class EducationItem {
+  description:string;
+  displayIdentifier:string = 'basic'
+  constructor({ description, displayIdentifier }: { description: string; displayIdentifier?: string; }){
+    this.description = description;
+    if(typeof displayIdentifier == 'undefined'){
+      this.displayIdentifier = 'basic';
+    }
+    else {
+      this.displayIdentifier = displayIdentifier;
+    }
+  }
 }
