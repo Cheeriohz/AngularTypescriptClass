67e6a07d2aeeb7c9363dbf9a67e8977b03b4c484
DougHomeDesktop <dwbono01@gmail.com>
2019-12-16 19:39:45 -0500

    added some new data, view, and bindings for hello world

--- AngularProjects/hello-world/src/app/app.component.ts
+++ AngularProjects/hello-world/src/app/app.component.ts
@@ -8,10 +8,20 @@ export class AppComponent implements OnInit{
   title = 'Hello, World!';
   givenName: string = 'Douglas Bonomo';
   myModel: AppModel = null;
-
+  classRoom: Student[] = [];
+  selectedStudent:Student = null;
   ngOnInit() {
-    //this.myModel = new AppModel("Gerico", 11);
+    this.myModel = new AppModel("Gerico", 11);
+    this.classRoom.push(new Student("John Josh", this.classRoom));
+    this.classRoom.push(new Student("Jill Jane", this.classRoom));
+    this.classRoom.push(new Student("Bill Bob", this.classRoom));
+    this.classRoom.push(new Student("Matt Mark", this.classRoom));
   }
+
+  onSelect(student: Student){
+    this.selectedStudent = student;
+  }
+
 }
 
 class AppModel {
@@ -23,5 +33,14 @@ class AppModel {
   }
   get toJson(): string {
     return `{ name: ${this.name}, age: ${this.age} }`;
+  }
+}
+
+class Student {
+  name: string ="";
+  id: number;
+  constructor(name: string, classRoom: Student[]) {
+    this.name = name;
+    this.id = Math.max(...classRoom.map(student => student.id), 0) + 1;
   }
 }
