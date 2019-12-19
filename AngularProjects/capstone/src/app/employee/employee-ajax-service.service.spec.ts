10a1839e5155486b6320d8c1e5e1bc7cc7dc3239
DougHomeDesktop <dwbono01@gmail.com>
2019-12-18 19:59:07 -0500

    fixed employee service name

--- AngularProjects/capstone/src/app/employee/employee-ajax-service.service.spec.ts
+++ AngularProjects/capstone/src/app/employee/employee-ajax-service.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EmployeeAjaxService } from './employee-service.service';
+import { EmployeeAjaxService } from './employee-ajax-service.service';
 
 describe('EmployeeServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
